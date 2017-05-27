a52d4af089e3fe683ca443b284673592ed3bc9f8
Krozo <denisrimeiko@gmail.com>
2017-05-27 20:19:42 +0300

    Added start information... with exercise 1

--- features/demo.feature
+++ features/demo.feature
@@ -1,9 +1,16 @@
 Feature: Demo Feature
 
-    Scenario: Open SignUp Page
-        Given I am on Appimation sign up page
-        When I submit signup details
+    # Scenario: Open SignUp Page
+    #     Given I am on Appimation sign up page
+    #     When I submit signup details
 
     # Scenario: I sumbit signup without entering password again
     #     Given I am on Appimation sign up page
     #     When I submit signup details without again password
+
+
+
+    Scenario - Login User
+        Given I am on Appimation login page
+        When I submit login details
+        Then I see project page
--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -1,12 +1,27 @@
-When(/^I am on Appimation sign up page$/) do
-    @tests.signup_tests.load_signup_page
+# When(/^I am on Appimation sign up page$/) do
+#     @tests.signup_tests.load_signup_page
+# end
+
+# When(/^I submit signup details$/) do
+#     @tests.signup_tests.submit_signup_details
+# end
+
+# When(/^I submit signup details without again password$/) do
+#     @tests.signup_tests.submit_signup_details_no_again_password
+# end
+
+
+# Exercise Nr.1
+Given(/^I am on Appimation login page$/) do
+    
 end
 
-When(/^I submit signup details$/) do
-    @tests.signup_tests.submit_signup_details
+When(/^I submit login details$/) do
+    
 end
 
-When(/^I submit signup details without again password$/) do
-    @tests.signup_tests.submit_signup_details_no_again_password
+Then(/^I see project page$/) do
+    
 end
 
+# Exercise Nr.2
